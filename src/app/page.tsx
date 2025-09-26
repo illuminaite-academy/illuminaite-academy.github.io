0ea040b33d53029103fdf95ce1446299e25a8066
tenko08 <unalai1027@gmail.com>
2025-09-26 17:26:01 -0400

    file structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import About from "@/components/about";
+import Contact from "@/components/contact";
+import Footer from "@/components/footer";
+import Hero from "@/components/hero";
+import Impact from "@/components/impact";
+import Initiatives from "@/components/initiatives";
+import Navbar from "@/components/navbar";
+
+
 export default function Main() {
     return (
         <div>
-            <h1>Main</h1>
+            <Navbar />
+            <Hero />
+            <About />
+            <Impact />
+            <Initiatives />
+            <Contact />
+            <Footer />
         </div>
     )
 }
